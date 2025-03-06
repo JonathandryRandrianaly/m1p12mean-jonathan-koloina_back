9e4e7add8ae65f6fc2d32aa7cf132bc5265b4d2e
koloina06 <koloina.mihajatiana@gmail.com>
2025-03-06 17:59:08 +0300

    feat: ajout de la gestion des accès en fonction des rôles de l'utilisateur

--- routes/roleRoute.js
+++ routes/roleRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const roleController = require('../controllers/roleController');
+const { authorizeRoles } = require('../middlewares/authMiddleware');
 
-router.post('/role', roleController.createRole);
-router.post('/role/:roleId', roleController.updateEtatRole);
+router.post('/role', authorizeRoles('manager'), roleController.createRole);
+router.post('/role/:roleId', authorizeRoles('manager'), roleController.updateEtatRole);
 
 module.exports = router;
