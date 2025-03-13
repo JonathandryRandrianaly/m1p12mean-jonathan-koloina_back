77f4b86bdceaeae8a3c21cc618b35021e6ef3f65
koloina06 <koloina.mihajatiana@gmail.com>
2025-03-13 11:40:30 +0300

    recherche et filtre consommable

--- app/services/searchService.js
+++ app/services/searchService.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Consommable = require('../models/Consommable');
+
 exports.searchModels = async ({ page = 1, limit = 10, sortedColumn = '', sortDirection = 'asc', nom = '',etats = []}, model) => {
     const defaultSortedColumn = sortedColumn || 'nom';
     const pageNumber = parseInt(page);
@@ -52,4 +55,44 @@ exports.searchRoles = async ({ page = 1, limit = 10, sortedColumn = '', sortDire
     });
 
     return { totalItems, items: models };
+};
+
+exports.searchConsommables = async ({ page = 1, limit = 10, sortedColumn = '', sortDirection = 'asc', nom = '',etats = [], unites= []}) => {
+    const defaultSortedColumn = sortedColumn || 'nom';
+    const pageNumber = parseInt(page);
+    const pageSize = parseInt(limit);
+    const sortOrder = sortDirection === 'desc' ? -1 : 1;
+
+    let query = {};
+    if (nom) {
+        query.nom = { $regex: nom, $options: 'i' };
+    }
+
+    if (etats.length > 0) {
+        query['etat.code'] = { $in: etats };  
+    }
+
+    if(unites.length >0){
+        query.unite = { $in: unites.map(uniteId => new mongoose.Types.ObjectId(uniteId)) };
+    }
+    const totalItems = await Consommable.countDocuments(query);
+    let consommables = await Consommable.find(query)
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+        .populate('unite');
+
+        consommables = consommables.sort((a, b) => {
+            if (defaultSortedColumn === 'unite') {
+                const roleA = a.unite?.nom.toLowerCase();
+                const roleB = b.unite?.nom.toLowerCase();
+                return roleA.localeCompare(roleB) * sortOrder;
+            } else {
+                const aValue = a[defaultSortedColumn] || '';
+                const bValue = b[defaultSortedColumn] || '';
+                return aValue.localeCompare(bValue) * sortOrder;
+            }
+
+    });
+
+    return { totalItems, items: consommables };
 };
