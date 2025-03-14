cb32c2e4c77cef1fa1bee1631e6bae6f182abb2d
Andry Jonathan Randrianaly <jonathanrandrianaly@gmail.com>
2025-03-14 17:53:55 +0300

    type entretien par categorie

--- app/routes/typeEntretienRoute.js
+++ app/routes/typeEntretienRoute.js
@@ -6,6 +6,7 @@ const typeEntretienController = require('../controllers/typeEntretienController'
 router.get('/type-entretiens', authorizeRoles('manager','client','mecanicien'), typeEntretienController.getAllTypeEntretien);
 router.get('/type-entretiens/search', authorizeRoles('manager','client','mecanicien'), typeEntretienController.searchTypesEntretien);
 router.get('/type-entretiens/statut/:statut', authorizeRoles('manager','client','mecanicien'), typeEntretienController.getAllTypeEntretienByStatut);
+router.get('/type-entretiens/categorie/:categorieId', authorizeRoles('manager','client','mecanicien'), typeEntretienController.getTypeEntretienByCategorie);
 
 router.post('/type-entretien', authorizeRoles('manager'), typeEntretienController.createTypeEntretien);
 router.post('/type-entretien/:typeEntretienId', authorizeRoles('manager'), typeEntretienController.updateEtatTypeEntretien);
