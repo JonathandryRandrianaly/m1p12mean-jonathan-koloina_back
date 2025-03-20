faf229f0d8be4cb68992acfb09e4f739a9afd0d8
Andry Jonathan Randrianaly <jonathanrandrianaly@gmail.com>
2025-03-20 14:30:35 +0300

    creation model facture et detail

---
+++ app/models/DetailFacture.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const detailFactureSchema = new mongoose.Schema({
+    facture: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Facture'
+    },
+    entretien: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Entretien'
+    },
+    prix: {
+        type: Number,
+        required: true
+    }
+},{ timestamps: true })
+
+module.exports = mongoose.model("DetailFacture", detailFactureSchema);
